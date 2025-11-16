d91d0ee8e2b805b0dc924a0b5f618a843dfde337
Cedrik Melendez <cedrikjames0405@gmail.com>
2025-11-15 21:31:17 -0800

    new navBar

--- navBar.js
+++ navBar.js
@@ -1,4 +1,6 @@
 document.getElementById("navbar").innerHTML = `
-<div class="flex justify-center py-3 bg-pink-400">
-    <img alt="ClashRoom full logo" src="./images/ClashRoomFullLogo.png" class="max-w-lg max-h-lg"></img>
+  <div class="flex justify-center py-3 bg-pink-400">
+    <a href="index.html">
+      <img alt="ClashRoom full logo" src="./images/ClashRoomFullLogo.png" class="max-w-lg max-h-lg" />
+    </a>
   </div>`;
